29a730ac546dd6a4cdc9887370bb827c8de7e076
Adam Murray <adamjmurray@gmail.com>
2008-11-10 08:42:26 +0000

    cleanup midi renderer
    git-svn-id: svn://quagmire/repo/max/externals/java/ruby/grammar@410 99bd3f52-245c-3848-8005-63e5fbd5b414

--- lib/cosy/renderer/midi_file_renderer.rb
+++ lib/cosy/renderer/midi_file_renderer.rb
@@ -11,6 +11,15 @@ module MIDI
       return event
     end
   end
+  
+  class Sequence
+    def create_track(name=nil)
+      track = MIDI::Track.new(self)
+      track.name = name if name
+      tracks << track
+      return track  
+    end
+  end
 end
 
 module Cosy
@@ -21,20 +30,18 @@ module Cosy
     def initialize
       super
       @midi_sequence = MIDI::Sequence.new()
+      @channel = 1
       @time = 0
       
-      @meta_track = add_track
-      @meta_track.events << MIDI::MetaEvent.new(MIDI::META_SEQ_NAME, 'Cosy Sequence')
+      @meta_track = @midi_sequence.create_track 'Cosy Sequence'
+      @track = @midi_sequence.create_track 'Track 1'
+      
       tempo(120)
-
-      @track = add_track
-      @channel = 1
       program(0)
     end
 
     def render(input, output_filename)
       parse input
-      @absolute_delta = delta_time = 0
       
       while event = next_event
         case event
@@ -44,33 +51,31 @@ module Cosy
         when Program then program(event.value)
 
         when NoteEvent
-          pitches, velocity, duration = event.pitches, event.velocity.to_i, event.duration.to_i
-          if duration < 0
-            delta_time = -duration
-          else
-            # TODO: special case duration 0?
-            render_note(pitches, velocity, delta_time, duration)
-            delta_time = 0
+          pitches, velocity, duration = event.pitches, event.velocity, event.duration
+          if duration >= 0
+            notes(pitches, velocity, duration)
+          else            
+            rest(-duration)
           end
           
-        else
-          raise 'Unsupported Event: #{event.inspect}'
-        end
+        else raise 'Unsupported Event: #{event.inspect}' end
       end
       
-      note_off(0, 0, 480) # pad the end a bit, otherwise seems to cut off (TODO: make this optional)  
+      # pad the end a bit, otherwise seems to cut off (TODO: make this optional)  
+      @time += 480
+      note_off(0, 0) 
       
       @meta_track.recalc_delta_from_times
       @track.recalc_delta_from_times
       
       print_midi
-      File.open(output_filename, 'wb'){ |file| @midi_sequence.write(file) }
+      File.open(output_filename, 'wb') { |file| @midi_sequence.write(file) }
     end
     
     def print_midi
       @midi_sequence.each do |track|
-        puts "*** track name \"#{track.name}\""
-        puts "instrument name \"#{track.instrument}\""
+        puts "\n*** track name \"#{track.name}\""
+        #puts "instrument name \"#{track.instrument}\""
         puts "#{track.events.length} events"
         track.each do |event|
           event.print_decimal_numbers = true # default = false (print hex)
@@ -82,12 +87,6 @@ module Cosy
 
     ############
     private  
-    
-    def add_track
-      track = MIDI::Track.new(@midi_sequence)
-      @midi_sequence.tracks << track
-      return track
-    end
     
     # Set tempo in terms of Quarter Notes per Minute (aka BPM)
     def tempo(qnpm)
@@ -101,31 +100,24 @@ module Cosy
        @track.insert(event, @time)
      end
     
-    def note_on(pitch, velocity, delta_time)
-      @time += delta_time
-      event = MIDI::NoteOnEvent.new(@channel, pitch, velocity)
+    def note_on(pitch, velocity)
+      event = MIDI::NoteOnEvent.new(@channel, pitch.to_i, velocity.to_i)
       @track.insert(event, @time)
     end
     
-    def note_off(pitch, velocity, delta_time)
-      @time += delta_time
-      event = MIDI::NoteOffEvent.new(@channel, pitch, velocity)
+    def note_off(pitch, velocity)
+      event = MIDI::NoteOffEvent.new(@channel, pitch.to_i, velocity.to_i)
       @track.insert(event, @time)
     end
     
-    def render_note(pitches, velocity, delta_time, duration)
-      pitches.each do |pitch|
-        pitch = pitch.to_i
-        note_on(pitch, velocity, delta_time)
-        delta_time = 0 # if we're playing a chord the next pitch has delta_time=0
-      end
-      
-      delta_time = duration
-      pitches.each do |pitch|
-        pitch = pitch.to_i
-        note_off(pitch, velocity, delta_time)  
-        delta_time = 0 # if we're playing a chord the next pitch has delta_time=0  
-      end
+    def notes(pitches, velocity, duration)
+      pitches.each { |pitch| note_on(pitch, velocity) }
+      @time += duration.to_i
+      pitches.each { |pitch| note_off(pitch, velocity) }
+    end
+    
+    def rest(duration)
+      @time += duration.to_i
     end
     
   end
@@ -134,4 +126,5 @@ end
 
 #Cosy::MidiRenderer.new.render 'TEMPO=60; e*4; TEMPO=120; d*8; TEMPO=240; c*16', 'test.mid'
 # Cosy::MidiRenderer.new.render 'TEMPO=60; c4:q c c c:1/5q*5 c4:w', 'test.mid'
-Cosy::MidiRenderer.new.render '((G4 F4 E4 D4)*4 C4):(q. i):(p mf ff)', 'test.mid'
+#Cosy::MidiRenderer.new.render '((G4 F4 E4 D4)*4 C4):(q. i):(p mf ff)', 'test.mid'
+Cosy::MidiRenderer.new.render 'c -q d:q', 'test.mid'
